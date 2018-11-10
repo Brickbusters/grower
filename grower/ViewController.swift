d57992faf7182d15358da0a4e2ec6cd94345d529
Dan Qu <scuba.dan@gmail.com>
2018-11-09 16:57:59 -0800

    Initial Commit

---
+++ grower/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  grower
+//
+//  Created by Diheng Qu on 11/9/18.
+//  Copyright © 2018 Diheng Qu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
